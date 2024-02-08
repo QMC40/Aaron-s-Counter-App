d56e42c4ef1b229d1aaeeb4b47dc6406699bb20f
panzer <afortner@islander.tamucc.edu>
2024-02-07 21:22:31 -0600

    basic app done, has facts and counter and passes basic tests IAW job sheet

--- app/src/main/java/com/example/myintroduction/MainActivity.kt
+++ app/src/main/java/com/example/myintroduction/MainActivity.kt
@@ -4,11 +4,34 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var digitCount: TextView
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        digitCount = findViewById(R.id.digitCount)
+        count = 0
+
+    }
+
+    fun incrementCount(view: View) {
+        count++
+        digitCount.text = count.toString()
+    }
+
+    fun decrementCount(view: View) {
+        count--
+        digitCount.text = count.toString()
+    }
+
+    fun resetCount(view: View) {
+        count = 0
+        digitCount.text = count.toString()
     }
 
     fun openMeActivity(view: View) {
@@ -25,5 +48,7 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, Tequila::class.java)
         startActivity(intent)
     }
+
+
 
 }
